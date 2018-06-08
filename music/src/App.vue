bd724ac60eb48e5c58a6abfb254a9875c42c57cf
walkerwan <2431136239@qq.com>
2018-06-08 21:56:07 +0800

    目录结构以及基本配置

---
+++ music/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style  lang = 'less' scoped rel="stylesheet/less">
+/* @import "~common/less/variable";
+#app {
+  color: @color-theme
+} */
+</style>
